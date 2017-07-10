d6d306fc9f84b9d8e1676f1e98c1860f7c926b3d
eventia <eventia@gmail.com>
2017-07-10 18:25:03 +0900

    Photo Blog 1st Commit

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d/img', null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
@@ -14,4 +15,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-        
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        super(Post, self).delete(*args, **kwargs)
